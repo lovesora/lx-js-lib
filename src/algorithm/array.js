248ec428bbc7a8f47b0942a4d1f9c1a100ad9633
liuxin <475212506@qq.com>
2017-11-10 19:49:00 +0800

    add lodash.clonedeep update array.js

--- src/algorithm/array.js
+++ src/algorithm/array.js
@@ -1,10 +1,13 @@
 import _forEach from 'lodash.foreach';
+import _cloneDeep from 'lodash.clonedeep';
 
-export function search(data, filterRules) {
+export function search(data, filterValue) {
+    let _filterValue = _cloneDeep(filterValue);
+    
     let SearchType = {
         AND: '&',
         OR: '|',
-    }
+    };
     
     let _data = data;
 
@@ -18,14 +21,14 @@ export function search(data, filterRules) {
      * @param cols 列名数组
      * @param filterValue 需要过滤的值 string | string[]
      */
-    let _filter = function (row, cols, filterValue) {
+    let _filter = function(row, cols, filterValue) {
         return cols.filter(colName => {
             if (filterValue === undefined || filterValue === null)
                 filterValue = '';
             return String(row[colName]).toLowerCase().indexOf(String(filterValue).toLowerCase()) > -1;
         }).length;
     };
-    let _filterAnd = function (row, cols, filterValue) {
+    let _filterAnd = function(row, cols, filterValue) {
         return cols.filter(colName => {
             if (filterValue === undefined || filterValue === null)
                 filterValue = '';
@@ -38,24 +41,27 @@ export function search(data, filterRules) {
             return String(row[colName]).toLowerCase().indexOf(String(filterValue).toLowerCase()) > -1;
         }).length;
     };
-    let _orFilter = function (data, rule) {
+    let _orFilter = function(data, rule) {
         return data.filter(v => {
-            if (Object.prototype.toString.call(rule.value) === '[object Array]') {
-                let value = rule.value;
+            if (Object.prototype.toString.call(rule.filterValue) === '[object Array]') {
+                let value = rule.filterValue;
                 let result = value.filter(_value => {
-                    return _filter(v, rule.cols || _defaultCols, _value) > 0
+                    return _filter(v, rule.cols || _defaultCols, _value) > 0;
                 }).length === value.length;
                 return result;
             } else {
-                return _filter(v, rule.cols || _defaultCols, rule.value) > 0
+                return _filter(v, rule.cols || _defaultCols, rule.filterValue) > 0;
             }
         });
     };
-    let _andFilter = function (data, rule) {
-        return data.filter(v => _filterAnd(v, rule.cols || _defaultCols, rule.value) === rule.cols.length);
+    let _andFilter = function(data, rule) {
+        return data.filter(v => _filterAnd(v, rule.cols || _defaultCols, rule.filterValue) === rule.cols.length);
     };
 
-    _forEach(filterOption, (option) => {
+
+    _forEach(_filterValue, (option) => {
+        option.rule.filterValue = option.rule.filterValue.split(' ');
+        
         switch (option.type) {
             case SearchType.AND:
                 {
@@ -71,4 +77,4 @@ export function search(data, filterRules) {
     });
 
     return _data;
-}
+};
